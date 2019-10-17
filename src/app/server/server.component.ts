a96e51611d97d021e33d79c759c41c3d48875561
o_idriss <synderime@gmail.com>
2019-10-17 03:03:48 +0000

    Training on basics

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl:'./server.component.html'
+})
+
+export class ServerComponent {
+    constructor() { }
+
+    serverId = 10;
+    serverStatus = 'Offline';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created !'
+  serverName = '';
+  
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = this.serverName + ' was created !'
+  }
+  
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
+    //console.log(event.target.value);
+    
+  }
+
+}
